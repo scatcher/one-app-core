93629d0d690b4c46da8569b5ed00d979a1890e9a
Scott <github@scotthatcher.com>
2014-03-07 11:36:56 -0800

    Fixed case where single digit day didn't add a leading 0

--- scripts/services/utility.js
+++ scripts/services/utility.js
@@ -277,7 +277,8 @@ angular.module('OneApp')
             var yyyy = date.getFullYear().toString();
             var mm = (date.getMonth()+1).toString();
             var dd = date.getDate().toString();
-            return parseInt(yyyy + (mm[1]?mm:"0"+mm[0]) + dd);
+            /** Add leading 0's to month and day if necessary */
+            return parseInt(yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]));
         }
 
         /**
