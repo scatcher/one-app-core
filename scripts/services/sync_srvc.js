cdab41819f47b3a09c2868419fa00dcdc6f64d04
Scott <github@scotthatcher.com>
2014-03-18 15:53:34 -0700

    Prevent sync from automatically calling updateData on a model.  Instead broadcast event to subscribers and let them take appropriate action.

--- scripts/services/sync_srvc.js
+++ scripts/services/sync_srvc.js
@@ -1,63 +1,62 @@
 'use strict';
 
 angular.module('OneApp')
-    .factory('syncService', function ($q, $timeout, $firebase, config, utility, dataService) {
+    .factory('syncService', function ($q, $timeout, $firebase, config) {
 
+        /** Constructor to handle notifying models when data is updated */
         function synchronizeData(model, updateQuery) {
-            console.time("Sync Service");
             var sync = {};
             sync.updateQuery = updateQuery;
             sync.changeNotifier = new Firebase(config.firebaseURL + '/changes/' + model.list.title);
             sync.lastUpdate = $firebase(sync.changeNotifier);
 
-            //Used to notify all other users that a change has been made
+            /** Notify all other users listening to this model that a change has been made */
             sync.registerChange = function () {
                 console.log("Change detected in " + model.list.title + ' list.');
                 var timeStamp = Firebase.ServerValue.TIMESTAMP;
+                /** Reset counter so change made by current user won't also cause a refresh */
+                sync.changeCount = 0;
                 sync.lastUpdate.$set(timeStamp);
             };
 
-            //Container to hold all current subscriptions
+            /** Container to hold all current subscriptions for the model */
             sync.subscriptions = [];
 
-            //Running counter of the number of changes
+            /** Running counter of the number of changes */
             sync.changeCount = 0;
 
-            sync.processChanges = function() {
-                //Prevent from running the first time
-                if(sync.changeCount > 0) {
-                    model.updateData().then(function() {
-                        _.each(sync.subscriptions, function(callback) {
-                            console.log("Processing callback");
-                            if(_.isFunction(callback)) {
-                                callback();
-                            }
-                        });
+            sync.processChanges = function () {
+                /** Prevent from running the first time and when most recent change was made by current user */
+                if (sync.changeCount > 0) {
+                    _.each(sync.subscriptions, function (callback) {
+                        console.log("Processing callback");
+                        if (_.isFunction(callback)) {
+                            callback();
+                        }
                     });
                 }
 
                 sync.changeCount += 1;
             };
 
-            /** Don't make a call more than once every 10 seconds */
-            sync.throttleRequests = _.throttle(function() {return sync.processChanges() }, 10000);
+            /** Don't make a call more than once every second */
+            sync.throttleRequests = _.throttle(function () {
+                return sync.processChanges()
+            }, 1000, {leading: false});
 
-            //Fired when anyone updates a requirement
-            sync.lastUpdate.$on("change", function(newVal, oldVal) {
+            /** Fired when anyone updates a list item */
+            sync.lastUpdate.$on("change", function (newVal, oldVal) {
                 sync.throttleRequests();
             });
 
 
-
-            //Allows controller to be notified when change is made
+            /** Allows subscribers (controllers) to be notified when change is made */
             sync.subscribeToChanges = function (callback) {
                 if (sync.subscriptions.indexOf(callback) === -1) {
-                    //Only register new subscriptions
+                    /** Only register new subscriptions, ignore if subscription already exists */
                     sync.subscriptions.push(callback);
                 }
             };
-
-            console.timeEnd("Sync Service");
 
             return sync;
         }
