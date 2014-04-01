9f1c177bffcdf8a715a8f79aa3730dc16aa6e46e
Scott Hatcher <scott@scotthatcher.com>
2014-04-01 07:52:53 -0700

    Set a watch to reevaluate when bound field is changed

--- app/scripts/directives/oa_select/oa_select_dir.js
+++ app/scripts/directives/oa_select/oa_select_dir.js
@@ -1,70 +1,45 @@
+'use strict';
+
 angular.module('OneApp')
-    .directive('oaSelect', function ($timeout) {
+    .directive('oaSelect', function () {
         return {
-            restrict: "A",
+            restrict: 'A',
             replace: true,
             template: '' +
-                '<span class="ng-cloak">\n' +
-                '   <span ng-if="!multi">\n' +
-                '       <select class="form-control" ng-model="state.singleSelectID"\n ' +
-                '           ng-change="updateSingleModel()" style="width: 100%" ng-disabled="ngDisabled"\n ' +
-                '           ng-options="lookup.id as lookup[state.lookupField] for lookup in arr">\n ' +
-                '       </select>\n' +
-                '   </span>\n' +
-                '   <span ng-if="multi">\n' +
-                '       <select multiple ui-select2 ng-model="state.multiSelectIDs"\n' +
-                '           ng-change="updateMultiModel()" style="width: 100%;" ng-disabled="ngDisabled">\n' +
-                '               <option></option>\n' +
-                '               <option ng-repeat="lookup in arr" value="{{ lookup.id }}"\n' +
-                '                   ng-bind="lookup[state.lookupField]">&nbsp;</option>\n' +
-                '       </select>\n' +
-                '   </span>\n' +
-                '</span>\n' +
+                '<span class="ng-cloak">' +
+                '   <span ng-if="!multi">' +
+                '       <select class="form-control" ng-model="state.singleSelectID" ' +
+                '           ng-change="updateSingleModel()" style="width: 100%" ng-disabled="ngDisabled" ' +
+                '           ng-options="lookup.id as lookup[state.lookupField] for lookup in arr"> ' +
+                '       </select>' +
+                '   </span>' +
+                '   <span ng-if="multi">' +
+                '       <select multiple ui-select2 ng-model="state.multiSelectIDs"' +
+                '           ng-change="updateMultiModel()" style="width: 100%;" ng-disabled="ngDisabled">' +
+                '               <option></option>' +
+                '               <option ng-repeat="lookup in arr" value="{{ lookup.id }}"' +
+                '                   ng-bind="lookup[state.lookupField]">&nbsp;</option>' +
+                '       </select>' +
+                '   </span>' +
+                '</span>' +
                 '',
             scope: {
                 bindedField: '=',   //The field on the model to bind to
                 multi: '=',         //Single select if not set or set to false
                 arr: '=',           //Array of lookup options
                 lookupValue: '=',   //Field name to map the lookupValue to (default: 'title')
-                changed: '=',       //Optional reference to a change notifier function
                 ngDisabled: '='     //Pass through to disable control using ng-disabled on element if set
             },
-            link: function (scope, element, attrs) {
+            link: function (scope) {
 
                 scope.state = {
                     multiSelectIDs: [],
                     singleSelectID: ''
                 };
 
-                //Default to title field if not provided
+                /** Default to title field if not provided */
                 scope.state.lookupField = scope.lookupValue || 'title';
-
-                $timeout(function() {
-
-                    if (scope.multi) {
-                        //Multi Select Mode
-                        //Set the string version of id's to allow multi-select control to work properly
-                        _.each(scope.bindedField, function (selectedLookup) {
-                            //Push id as a string to match what Select2 is expecting
-                            scope.state.multiSelectIDs.push(selectedLookup.lookupId.toString());
-                        });
-                    } else {
-                        //Single Select Mode
-                        if (_.isObject(scope.bindedField) && scope.bindedField.lookupId) {
-                            //Set the selected id as string
-                            scope.state.singleSelectID = scope.bindedField.lookupId;
-                        }
-                    }
-                }, 0);
-
-                //Optionally callback if provided
-                if(_.isFunction(scope.changed)) {
-                    scope.$watch('bindedField', function(newVal, oldVal) {
-                        if(newVal === oldVal) return;
-                        scope.changed();
-                    }, true);
-                }
-
+                
                 var buildLookupObject = function(stringId) {
                     var intID = parseInt(stringId, 10);
                     var match = _.findWhere(scope.arr, {id: intID});
@@ -86,22 +61,41 @@ angular.module('OneApp')
                 };
 
                 scope.updateMultiModel = function () {
-                    //Ensure field being binded against is array
+                    /** Ensure field being binded against is array */
                     if (!_.isArray(scope.bindedField)) {
                         scope.bindedField = [];
                     }
-                    //Clear out existing contents
+                    /** Clear out existing contents */
                     scope.bindedField.length = 0;
-                    //Push formatted lookup object back
+                    /** Push formatted lookup object back */
                     _.each(scope.state.multiSelectIDs, function (stringId) {
                         scope.bindedField.push(buildLookupObject(stringId));
                     });
                 };
 
                 scope.updateSingleModel = function () {
-                    //Create an object with expected lookupId/lookupValue properties
+                    /** Create an object with expected lookupId/lookupValue properties */
                     scope.bindedField = buildLookupObject(scope.state.singleSelectID);
                 };
+
+                /** Process initially and whenever the underlying value is changed */
+                scope.$watch('bindedField', function() {
+                    if (scope.multi) {
+                        /** Multi Select Mode
+                         *  Set the string version of id's to allow multi-select control to work properly */
+                        _.each(scope.bindedField, function (selectedLookup) {
+                            /** Push id as a string to match what Select2 is expecting */
+                            scope.state.multiSelectIDs.push(selectedLookup.lookupId.toString());
+                        });
+                    } else {
+                        /** Single Select Mode */
+                        if (_.isObject(scope.bindedField) && scope.bindedField.lookupId) {
+                            /** Set the selected id as string */
+                            scope.state.singleSelectID = scope.bindedField.lookupId;
+                        }
+                    }
+                });
+
             }
         };
     });
