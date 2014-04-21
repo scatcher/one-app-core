57c1097ce0211368dabb961b71a22c7c9fab9369
Scott Hatcher <scott@scotthatcher.com>
2014-04-21 08:16:28 -0700

    Removed extra dependencies, still need to remove more

--- dist/sp-angular.js
+++ dist/sp-angular.js
@@ -10,25 +10,8 @@ angular.module('spAngular', [
   '$stateProvider',
   '$urlRouterProvider',
   function ($stateProvider, $urlRouterProvider) {
-    // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise('/');
-    // Now set up the states
-    $stateProvider.state('home', {
-      url: '/',
-      templateUrl: 'modules/main/views/main_view.html',
-      controller: 'mainCtrl'
-    }).state('groupmanager', {
-      url: '/group_manager',
-      templateUrl: 'bower_components/sp-angular/modules/group_manager/views/group_manager_view.html',
-      controller: 'groupManagerCtrl'
-    }).state('offline', {
-      url: '/offline',
-      templateUrl: 'bower_components/sp-angular/modules/dev/views/generate_offline_view.html',
-      controller: 'generateOfflineCtrl'
-    });
   }
 ]).run(function () {
-  console.log('Injector done loading all modules.');
 });
 ;
 'use strict';
